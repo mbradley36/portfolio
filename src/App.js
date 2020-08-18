f4425234ba51a9e496b50eee59c02097383e02b9
Miranda <mirandajhuet@gmail.com>
2020-08-18 09:54:57 -0500

    feat: scroll to top on new page, activated service worker

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Portfolio from "./components/Portfolio";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Portfolio />
     </Router>
   );
