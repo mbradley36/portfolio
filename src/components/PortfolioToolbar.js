4df7b8e0cae7584fa975a3281a7ca72351c6c468
Miranda <mirandajhuet@gmail.com>
2022-10-28 14:58:25 -0700

    chore: removed unused import

--- src/components/PortfolioToolbar.js
+++ src/components/PortfolioToolbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import AppBar from "@material-ui/core/AppBar";
